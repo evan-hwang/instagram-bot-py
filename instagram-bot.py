6599fd7f197b1429e513db2afe7f063e3b9f4ecf
Evan Hwang <evan.h@kakaocorp.com>
2018-12-26 15:30:07 +0900

    instagram login 구현

--- instagram-bot.py
+++ instagram-bot.py
@@ -2,13 +2,33 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-driver = webdriver.Chrome('/usr/local/bin/chromedriver')
-# 암묵적으로 웹 자원 로드를 위해 3초까지 기다려 준다.
-driver.implicitly_wait(3)
-# 브라우저를 켜본다.
-driver.get('https://nid.naver.com/nidlogin.login')
-# 아이디/비밀번호를 입력해준다.
-driver.find_element_by_name('id').send_keys('naver_id')
-driver.find_element_by_name('pw').send_keys('naver_password')
-# 로그인 버튼을 눌러주자.
-driver.find_element_by_xpath('//*[@id="frmNIDLogin"]/fieldset/input').click()
+
+class InstagramBot:
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+
+    def closeBrowser(self):
+        self.driver.close()
+
+    def login(self):
+        driver = self.driver
+        driver.get('https://www.instagram.com/')
+        time.sleep(3)
+
+        login_button = driver.find_element_by_xpath("//a[@href='/accounts/login/?source=auth_switcher']")
+        login_button.click()
+        time.sleep(3)
+        user_name_elem = driver.find_element_by_xpath("//input[@name='username']")
+        user_name_elem.clear()
+        user_name_elem.send_keys(self.username)
+        passworword_elem = driver.find_element_by_xpath("//input[@name='password']")
+        passworword_elem.clear()
+        passworword_elem.send_keys(self.password)
+        passworword_elem.send_keys(Keys.RETURN)
+
+    
+bot = InstagramBot('id', 'pw')
+bot.login()

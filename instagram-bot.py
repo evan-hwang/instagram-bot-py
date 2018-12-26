c6f119fccee5a514f9b46f15d40e4222074b133d
Evan Hwang <evan.h@kakaocorp.com>
2018-12-26 14:58:14 +0900

    Naver 로그인으로 테스트 해보기

--- instagram-bot.py
+++ instagram-bot.py
@@ -3,15 +3,12 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+# 암묵적으로 웹 자원 로드를 위해 3초까지 기다려 준다.
 driver.implicitly_wait(3)
-driver.get('https://google.com')
-
-# class InstagramBot:
-
-#     def __init__(self, username: str, password: str) -> None:
-#         self.username = username
-#         self.password = password
-#         self.driver = webdriver.Chrome()
-
-#     def closeBrowser(self):
-#         self.driver.close()
+# 브라우저를 켜본다.
+driver.get('https://nid.naver.com/nidlogin.login')
+# 아이디/비밀번호를 입력해준다.
+driver.find_element_by_name('id').send_keys('naver_id')
+driver.find_element_by_name('pw').send_keys('naver_password')
+# 로그인 버튼을 눌러주자.
+driver.find_element_by_xpath('//*[@id="frmNIDLogin"]/fieldset/input').click()
